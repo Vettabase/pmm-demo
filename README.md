de6ed693720d1a3c9f1721500ca1f57b180a9d2a
rcbensley <richardbensley@gmail.com>
2024-04-19 13:09:31 +0100

    Sync final

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 # PMM Demo
 
-PMM users
-thee meaning of each dashboard
-main MariaDB metrics
-which metrics you check first if there is a problem
-how you zoom in a time span to investigate a problem, the stats written below every graph
-query analytics, how they are ordere by default, how to order them differently, perhaps adding more columns
-
 ## Run
 
     docker compose up -d
 
 ## PMM console
 
-    127.0.0.1:
+    https://localhost
--- setup/setup.sql
+++ setup/setup.sql
@@ -1,4 +1,11 @@
-grant select, process, binlog monitor on *.* to 'pmm'@'%' identified by 'dba';
+grant select, process, replication client, reload on *.* to 'pmm'@'%' identified by 'dba' WITH MAX_USER_CONNECTIONS 10;
+
 grant all privileges on *.* to 'dba'@'%' identified by 'dba';
 
-create database sbtest;
+create database if not exists sbtest;
+
+UPDATE performance_schema.setup_consumers
+SET ENABLED = 'YES' WHERE NAME LIKE '%statements%';
+SET GLOBAL innodb_monitor_enable = all;
+
+
